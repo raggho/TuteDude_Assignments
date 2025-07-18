ab60e708c1352d9956e27940a4e85b0c29298afd
Rajesh Ghosh <37010442+raggho@users.noreply.github.com>
2025-07-18 21:31:18 +0530

    Update and rename Task_1 to Task_1.py

---
+++ Task_1.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the First number: "))
+b = int(input("Enter the Second number: "))
+
+print("Addition: ", a + b)
+print("Subtraction: ", a - b)
+print("Multiplication: ", a * b)
+print("Division: ", a / b)
