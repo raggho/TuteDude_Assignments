96894bc5a7597e7fc9f46bb9c27a3a9cfbd7afda
Rajesh Ghosh <37010442+raggho@users.noreply.github.com>
2025-07-20 20:18:51 +0530

    Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,4 +1,4 @@
-f_name = input("Enter the first name: ")
-s_name = input("Enter the Second name: ")
-
-print(f"Hello, {f_name} {s_name}! welcome to the python program.")
+sum = 0
+for i in range(1,51):
+    sum+=i
+print(f"The sum of numbers from 1 to 50 is:",sum)
