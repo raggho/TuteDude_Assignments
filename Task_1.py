ebba1fdd70457eee836e28c1ca266c445e67b146
Rajesh Ghosh <37010442+raggho@users.noreply.github.com>
2025-07-23 01:13:24 +0530

    Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,5 +1,7 @@
-n=int(input("Enter a number: "))
-if n % 2 == 0:
-    print(f"{n} is an even number")
-else:
-    print(f"{n} is an odd number")
+def factorial(n):
+    if n > 1:
+        fact = 1
+        for i in range(1,n+1):
+            fact *= i   
+    return f"Factorial of {n} is : {fact}"
+print(factorial(int(input("Enter the number: "))))
