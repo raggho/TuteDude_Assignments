c3277044156a2b05e4a34619a8ad99ccbbc5da71
Rajesh Ghosh <37010442+raggho@users.noreply.github.com>
2025-07-23 01:16:01 +0530

    Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -3,5 +3,7 @@ def factorial(n):
         fact = 1
         for i in range(1,n+1):
             fact *= i   
+    else:
+        fact = 1
     return f"Factorial of {n} is : {fact}"
 print(factorial(int(input("Enter the number: "))))
